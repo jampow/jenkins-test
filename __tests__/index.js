24bef3eab1863f5232a07d2eefe445bb3b02c78c
Gianpaulo M. Soares <gsoares@uolinc.com>
2017-07-17 14:05:50 -0300

    fixing tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -2,6 +2,6 @@ const sum = require('../index.js')
 
 describe('sum module test', () => {
 	it('should be 3 when passed 1 and 2', () => {
-		expect(sum(2,2)).toBe(3)
+		expect(sum(1,2)).toBe(3)
 	})
 })
